513b2fd61f196a0016eecc5b3a433e5b6c6bc103
Sarthak Goyal <sarthakgoyal183@gmail.com>
2020-02-09 12:30:13 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/datepicker/MainActivity.java
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Calendar cal = Calendar.getInstance();
-
                 int year = cal.get(Calendar.YEAR);
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
